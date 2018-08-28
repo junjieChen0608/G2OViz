c7b339445a63984d3b272a69b681ef52e6eaa412
jjchen <wiiso853@gmail.com>
2018-08-29 00:03:27 +0800

    try to overhaul old implementation

--- public/client.js
+++ public/client.js
@@ -63,6 +63,209 @@ function queryGraph(graphName, batchSize, iteration) {
 		// TODO consume response here to render
 		// vertexArray.push.apply(vertexArray, JSON.parse(JSON.stringify(responseJSON)));
 		// vertexArray = JSON.parse(JSON.stringify(responseJSON));
+
+		/******************************************* crazy *******************************************/
+
+			var container, stats;
+			var camera, controls, scene, renderer;
+			var pickingData = [], pickingTexture, pickingScene;
+			var highlightBox;
+
+			var mouse = new THREE.Vector2();
+			var offset = new THREE.Vector3( 10, 10, 10 );
+
+			init();
+			animate();
+
+			function init() {
+
+				container = document.getElementById( "container" );
+
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 100000 );
+				camera.position.z = 1000;
+
+				controls = new THREE.TrackballControls( camera );
+				controls.rotateSpeed = 1.0;
+				controls.zoomSpeed = 1.2;
+				controls.panSpeed = 0.8;
+				controls.noZoom = false;
+				controls.noPan = false;
+				controls.staticMoving = true;
+				controls.dynamicDampingFactor = 0.3;
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0xffffff );
+
+				pickingScene = new THREE.Scene();
+				pickingTexture = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight );
+				pickingTexture.texture.minFilter = THREE.LinearFilter;
+
+				scene.add( new THREE.AmbientLight( 0x555555 ) );
+
+				var light = new THREE.SpotLight( 0xffffff, 1.5 );
+				light.position.set( 0, 500, 2000 );
+				scene.add( light );
+
+				var pickingMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+				var defaultMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true, vertexColors: THREE.VertexColors, shininess: 0	} );
+
+				function applyVertexColors( geometry, color ) {
+
+					var position = geometry.attributes.position;
+					var colors = [];
+
+					for ( var i = 0; i < position.count; i ++ ) {
+
+						colors.push( color.r, color.g, color.b );
+
+					}
+
+					geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+
+				}
+
+				var geometriesDrawn = [];
+				var geometriesPicking = [];
+
+				var matrix = new THREE.Matrix4();
+				var quaternion = new THREE.Quaternion();
+				var color = new THREE.Color();
+
+				for ( var i = 0; i < 10000; i ++ ) {
+
+					var geometry = new THREE.ConeBufferGeometry(2.0, 15, 8, 1, false, 0, 6.3);
+					var position = new THREE.Vector3();
+					position.x = Math.random() * 10000 - 5000;
+					position.y = Math.random() * 6000 - 3000;
+					position.z = Math.random() * 8000 - 4000;
+
+					var rotation = new THREE.Euler();
+					rotation.x = Math.random() * 2 * Math.PI;
+					rotation.y = Math.random() * 2 * Math.PI;
+					rotation.z = Math.random() * 2 * Math.PI;
+
+					var scale = new THREE.Vector3(10, 10, 10);
+					// scale.x = Math.random() * 200 + 100;
+					// scale.y = Math.random() * 200 + 100;
+					// scale.z = Math.random() * 200 + 100;
+
+					quaternion.setFromEuler( rotation, false );
+					matrix.compose( position, quaternion, scale );
+
+					geometry.applyMatrix( matrix );
+
+					// give the geometry's vertices a random color, to be displayed
+
+					applyVertexColors( geometry, color.setHex( Math.random() * 0xffffff ) );
+
+					geometriesDrawn.push( geometry );
+
+					geometry = geometry.clone();
+					// give the geometry's vertices a color corresponding to the "id"
+
+					applyVertexColors( geometry, color.setHex( i ) );
+
+					geometriesPicking.push( geometry );
+
+					pickingData[ i ] = {
+
+						position: position,
+						rotation: rotation,
+						scale: scale
+
+					};
+
+				}
+
+				var objects = new THREE.Mesh( THREE.BufferGeometryUtils.mergeBufferGeometries( geometriesDrawn ), defaultMaterial );
+				scene.add( objects );
+
+				pickingScene.add( new THREE.Mesh( THREE.BufferGeometryUtils.mergeBufferGeometries( geometriesPicking ), pickingMaterial ) );
+
+				highlightBox = new THREE.Mesh(
+					new THREE.ConeBufferGeometry(2.0, 15, 8, 1, false, 0, 6.3),
+					new THREE.MeshLambertMaterial( { color: 0xffff00 }
+				) );
+				scene.add( highlightBox );
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				stats = new Stats();
+				container.appendChild( stats.dom );
+
+				renderer.domElement.addEventListener( 'mousemove', onMouseMove );
+
+			}
+
+			//
+
+			function onMouseMove( e ) {
+
+				mouse.x = e.clientX;
+				mouse.y = e.clientY;
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+				render();
+				stats.update();
+
+			}
+
+			function pick() {
+
+				//render the picking scene off-screen
+
+				renderer.render( pickingScene, camera, pickingTexture );
+
+				//create buffer for reading single pixel
+
+				var pixelBuffer = new Uint8Array( 4 );
+
+				//read the pixel under the mouse from the texture
+
+				renderer.readRenderTargetPixels( pickingTexture, mouse.x, pickingTexture.height - mouse.y, 1, 1, pixelBuffer );
+
+				//interpret the pixel as an ID
+
+				var id = ( pixelBuffer[ 0 ] << 16 ) | ( pixelBuffer[ 1 ] << 8 ) | ( pixelBuffer[ 2 ] );
+				var data = pickingData[ id ];
+				if ( data) {
+					//move our highlightBox so that it surrounds the picked object
+
+					if ( data.position && data.rotation && data.scale ){
+
+						highlightBox.position.copy( data.position );
+						highlightBox.rotation.copy( data.rotation );
+						highlightBox.scale.copy( data.scale ).add( offset );
+						highlightBox.visible = true;
+
+					}
+
+				} else {
+
+					highlightBox.visible = false;
+
+				}
+
+			}
+
+			function render() {
+
+				controls.update();
+
+				pick();
+
+				renderer.render( scene, camera );
+
+			}
+
+		/******************************************* crazy *******************************************/
 		console.log("vertex array length " + vertexArray.length);
 		if (++iteration < totalIteration) {
 			queryGraph(graphName, batchSize, iteration);
@@ -98,8 +301,8 @@ var colorOnSelect = 0xefdc04;
 
 var startTime = Date.now();
 // TODO render the scene batch by batch
-init();
-animate();
+// init();
+// animate();
 
 function initControls() {
 	controls = new THREE.TrackballControls(camera, renderer.domElement);
